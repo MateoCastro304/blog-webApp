71f3ffe3dea54be09bc992426439f7a3a8df88d7
MateoCastro304 <matecr361@gmail.com>
2026-01-31 16:39:46 -0300

    Primer diseño de home

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import bodyParser from 'body-parser'
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.render('index.ejs');
+})
+
+app.get('/create', (req,res) => {
+    res.render('create.ejs');
+})
+
+app.get('/about', (req,res) => {
+    res.render('about.ejs');
 })
 
 app.listen(port, ()=> {
